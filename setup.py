d48d4c3eb788ae5b3e67d623786600548659966d
Tim Vink <vinktim@gmail.com>
2020-03-01 21:42:42 +0100

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mkdocs-git-authors-plugin',
-    version='0.1.2',
+    version='0.2.0',
     description='Mkdocs plugin to display git authors of a page',
     long_description=long_description,
     long_description_content_type="text/markdown",
