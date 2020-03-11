7329984d028cc2d107fd9c6177cf70b80c584357
Urs Liska <git@ursliska.de>
2020-03-11 21:08:40 +0100

    Update unit test for git classes (#28)
    * Missing 'logging' import
    
    * Remove invalid escape string warning
    
    * Revert "Missing 'logging' import"
    
    This reverts commit 1a4e84f376a6537ff23b98a1c013213b81cf8bc3.

--- mkdocs_git_authors_plugin/git/page.py
+++ mkdocs_git_authors_plugin/git/page.py
@@ -150,7 +150,7 @@ class Page(AbstractRepoObject):
             --- (this method works through side effects)
         """
 
-        re_sha = re.compile('^\w{40}')
+        re_sha = re.compile(r'^\w{40}')
 
         cmd = GitCommand('blame', ['--porcelain', str(self._path)])
         cmd.run()
